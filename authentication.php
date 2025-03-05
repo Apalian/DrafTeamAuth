2baf827ee23bf7698501b67a85997eabf495d310
Colin_lsplt <colinlespi@gmail.com>
2025-03-05 16:37:12 +0100

    Update authentication.php

--- authentication.php
+++ authentication.php
@@ -8,7 +8,7 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-$secret = 'your-256-bit-secrete';
+$secret = 'your-256-bit-secret';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
     http_response_code(405);
